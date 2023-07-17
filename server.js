dc638889313e4d0994185de93f92ced50daf7692
MOHAMMED THOUFEEK A <86734714+mohammedthoufeek@users.noreply.github.com>
2023-07-17 21:05:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const itemRoutes = require('./routes/items.js');
+const itemRoutes = require('./routes/Items.js');
 const cors = require("cors")
 
 const app = express();
